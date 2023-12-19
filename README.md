775655d4f7d052900332f58f1121e1ba4342ae2f
subhadipbhowmik <shubhadipbhowmik02@gmail.com>
2023-12-19 19:29:23 +0530

    dev.to profile blog posts fetcher

---
+++ README.md
@@ -0,0 +1,23 @@
+## Dev.to Users Blog Post Fetcher
+
+This project is a simple web application that allows users to fetch blog posts from Dev.to based on a provided username.
+
+## Features
+
+- Fetch blog posts by entering a Dev.to username.
+- Display fetched blog posts with cover images, titles, descriptions, user information, tags, and reaction counts.
+- Responsive layout for different screen sizes.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript (ES6)
+
+## Usage
+
+1. Clone the repository.
+
+   ```bash
+   git clone https://github.com/subhadipbhowmik/dev.to-blog-search.git
+   ```
---
+++ script.js
@@ -0,0 +1,52 @@
+document
+  .getElementById("usernameForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("usernameInput").value;
+    const apiUrl = `https://dev.to/api/articles?username=${username}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      displayBlogPosts(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  });
+
+function displayBlogPosts(posts) {
+  const blogPostsContainer = document.getElementById("blogPosts");
+  blogPostsContainer.innerHTML = "";
+
+  const blogPostsGrid = document.createElement("div");
+  blogPostsGrid.classList.add("blog-posts-container");
+
+  posts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.classList.add("blog-post");
+
+    postElement.innerHTML = `
+      <img src="${post.cover_image}" alt="Cover Image">
+      <h2>${post.title}</h2>
+      <p>${post.description}</p>
+      <div class="user-info">
+        <img src="${
+          post.user.profile_image
+        }" alt="Profile Image" class="profile-image">
+        <p>${post.user.name}</p>
+      </div>
+      <p><b>${post.tag_list.join(", ")}</b></p>
+      <p>Reactions: ❤️ ${post.public_reactions_count}</p>
+      <a href="${post.url}" target="_blank" class="read-more-btn">Read More</a>
+    `;
+
+    blogPostsGrid.appendChild(postElement);
+  });
+
+  blogPostsContainer.appendChild(blogPostsGrid);
+  blogPostsContainer.classList.remove("hidden");
+}
+
+//
